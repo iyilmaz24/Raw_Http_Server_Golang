b289172096b7e71d82e93b013ef88e1383beb7f6
iyilmaz24 <iy.usa@outlook.com>
2025-09-21 01:21:59 -0400

    refactor file reading logic into TCP connection logic

--- main.go
+++ main.go
@@ -1,39 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 	"strings"
 )
 
+const port = ":4200"
+
 func main() {
-	file, err := os.Open("messages.txt");
+	listener, err := net.Listen("tcp", port)
+	fmt.Println("Server is listening on port", port)
 
 	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
+		log.Fatalf("Error opening TCP connection: %v", err)
 	}
-	defer file.Close();
-	
-	linesChannel := getLinesChannel(file);
-	for currString := range linesChannel {
-		if len(currString) > 0 {
-			log.Printf("read: %s\n", currString);
+	defer listener.Close()
+	var conn net.Conn	
+
+	for {
+		conn, err = listener.Accept()
+		if err != nil {
+			log.Fatalf("Error accepting connection: %v", err.Error())
 		}
+		fmt.Printf("Accepted connection from %v", conn.RemoteAddr())
+
+		linesChannel := getLinesChannel(conn)
+		for currentLine := range linesChannel {
+			if len(currentLine) > 0 {
+				fmt.Printf("read: %s\n", currentLine)
+			}
+		}
+		fmt.Println("Connection to", conn.RemoteAddr(), "closed")
 	}
 }
 
-func getLinesChannel(file io.ReadCloser) <-chan string {
-	var currString string;
-	var parts []string;
+func getLinesChannel(connection io.ReadCloser) <-chan string {
+	var currString string
+	var parts []string
 
 	data := make([]byte, 8);
 	linesChannel := make(chan string);
 
 	go func() {
+		defer connection.Close()
 		defer close(linesChannel)
 		for {
-			n, err := file.Read(data);
+			n, err := connection.Read(data)
 			if err != nil || n == 0 {
 				break
 			}
